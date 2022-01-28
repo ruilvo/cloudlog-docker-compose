8a84cc3d3d756671277e7a2f55f1a285563b6855
Rui Oliveira <ruimail24@gmail.com>
2022-01-28 13:17:04 +0000

    Fix updating from git

--- cloudlog/entrypoint.sh
+++ cloudlog/entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-if cd /cloudlog; then git pull; else git clone https://github.com/magicbug/Cloudlog.git /cloudlog; fi
+DIR="/cloudlog"
+# look for empty dir
+if [ -d "$DIR" ]
+then
+	if [ "$(ls -A $DIR)" ]; then
+        cd $DIR
+        git pull
+	else
+        git clone https://github.com/magicbug/Cloudlog.git /cloudlog
+	fi
+else
+	git clone https://github.com/magicbug/Cloudlog.git /cloudlog
+fi
+
 
 # Stop-gap, temporary solution
 chmod -R 777 /cloudlog
