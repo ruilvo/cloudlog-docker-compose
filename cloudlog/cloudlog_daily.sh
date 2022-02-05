6c97772491fa61c0099e09c0d4560d14f61dd128
Rui Oliveira <ruimail24@gmail.com>
2022-02-05 10:32:29 +0000

    Make permissions looser so the system works

--- cloudlog/cloudlog_daily.sh
+++ cloudlog/cloudlog_daily.sh
@@ -1,6 +1,12 @@
 #! /bin/bash
 
-# Update the Cloudlog installation every day at midnight
+
 cd /var/www/html
+# Update the Cloudlog installation every day at midnight
 /var/www/html/update_cloudlog.sh
+# Restore the permissions as they were before
+chown -R root:www-data /var/www/html
+chmod -R g+rw /var/www/html
+chmod +x /var/www/html/update_cloudlog.sh
+rm -rf /var/www/html/install
 cd $(dirname $0)
